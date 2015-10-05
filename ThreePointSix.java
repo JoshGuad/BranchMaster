37d40d5031623544ac509c64a37e1726d6612b13
JoshGuad <jguad.sv@gmail.com>
2015-10-05 13:48:01 -0400

    Update ThreePointSix.java

--- ThreePointSix.java
+++ ThreePointSix.java
@@ -3,18 +3,20 @@ public class ThreePointSix {
 	public enum MonthPicked {June, July, August, September, October, November, December, January, February, March, April, May}
 	public static void main(String[] args) {
 		MonthPicked [] choices = {MonthPicked.June, MonthPicked.July, MonthPicked.August, MonthPicked.September, MonthPicked.October, MonthPicked.November, MonthPicked.December, MonthPicked.January, MonthPicked.February, MonthPicked.March, MonthPicked.April, MonthPicked.May};
+	
+		while(true){
 		MonthPicked select = (MonthPicked) JOptionPane.showInputDialog(null, "Select your Month.", "Month Picked", JOptionPane.QUESTION_MESSAGE, null, choices, choices[11]);
 
 
 		switch(select) {
 		case June :
-			JOptionPane.showMessageDialog(null, "It’s a summer time. ");
+			JOptionPane.showMessageDialog(null, "Itâ€™s a summer time. ");
 			break;
 		case July :
-			JOptionPane.showMessageDialog(null, "It’s a summer time. ");
+			JOptionPane.showMessageDialog(null, "Itâ€™s a summer time. ");
 			break;
 		case August :
-			JOptionPane.showMessageDialog(null, "It’s a summer time. ");
+			JOptionPane.showMessageDialog(null, "Itâ€™s a summer time. ");
 			break;
 		case September :
 			JOptionPane.showMessageDialog(null, "Welcome to the foliage season! ");
@@ -46,6 +48,7 @@ public class ThreePointSix {
 		default:
 			JOptionPane.showMessageDialog(null, "");
 		
+		}
 		}
 	}
 }
